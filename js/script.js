42a1e1014398e8849b5255e04cd122d092706252
Hermes Strassacapa Junior <strassacapa.junior@gmail.com>
2023-03-10 22:17:39 -0300

    Accordion transformed in class

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 import SoftScroll from './modules/soft_scroll.js';
-import initAccordion from './modules/accordion.js';
+import Accordion from './modules/accordion.js';
 import initTabNavigation from './modules/tab_navigation.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
@@ -13,7 +13,9 @@ import initScrollAnimation from './modules/scroll_animation.js';
 const softScroll = new SoftScroll('[data-menu="soft"] a[href^="#"]');
 softScroll.init();
 
-initAccordion();
+const accordion = new Accordion('[data-animation="accordion"] dt');
+accordion.init();
+
 initTabNavigation();
 initModal();
 initTooltip();
