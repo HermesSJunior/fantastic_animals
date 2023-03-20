8e18597f24c515457dc296f8d4a5a2f80056c442
Hermes Strassacapa Junior <strassacapa.junior@gmail.com>
2023-03-20 10:45:06 -0300

    ScrollAnimation transformed in class

--- js/modules/scroll_animation.js
+++ js/modules/scroll_animation.js
@@ -2,23 +2,42 @@ export default class ScrollAnimation {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
     this.windowHalf = window.innerHeight * 0.6;
-    this.animationScroll = this.animationScroll.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
   }
 
-  animationScroll() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = (sectionTop - this.windowHalf) < 0;
-      if (isSectionVisible) {
-        section.classList.add('active');
-      } else if (section.classList.contains('active')) {
-        section.classList.remove('active');
+  // Get the distance each item site' scroll
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.windowHalf),
+      };
+    });
+  }
+
+  // Verify the distance in each object site' scroll
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add('active');
+      } else if (item.element.classList.contains('active')) {
+        item.element.classList.remove('active');
       }
     });
   }
 
   init() {
-    this.animationScroll();
-    window.addEventListener('scroll', this.animationScroll);
+    if (this.sections.length) {
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener('scroll', this.checkDistance);
+    }
+    return this;
+  }
+
+  // Remove the event scroll
+  stop() {
+    window.removeEventListener('scroll', this.checkDistance);
   }
 }
