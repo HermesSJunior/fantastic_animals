66924a8d31e2384b986225f56db1e93fbc564a48
Hermes Strassacapa Junior <strassacapa.junior@gmail.com>
2023-03-20 10:10:44 -0300

    scroll_animation refactored with class

--- js/modules/scroll_animation.js
+++ js/modules/scroll_animation.js
@@ -1,11 +1,14 @@
-export default function initScrollAnimation() {
-  const sections = document.querySelectorAll('[data-animation="scroll"]');
-  const windowHalf = window.innerHeight * 0.6;
+export default class ScrollAnimation {
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections);
+    this.windowHalf = window.innerHeight * 0.6;
+    this.animationScroll = this.animationScroll.bind(this);
+  }
 
-  function animationScroll() {
-    sections.forEach((section) => {
+  animationScroll() {
+    this.sections.forEach((section) => {
       const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = (sectionTop - windowHalf) < 0;
+      const isSectionVisible = (sectionTop - this.windowHalf) < 0;
       if (isSectionVisible) {
         section.classList.add('active');
       } else if (section.classList.contains('active')) {
@@ -14,8 +17,8 @@ export default function initScrollAnimation() {
     });
   }
 
-  if (sections.length) {
-    animationScroll();
-    window.addEventListener('scroll', animationScroll);
+  init() {
+    this.animationScroll();
+    window.addEventListener('scroll', this.animationScroll);
   }
 }
